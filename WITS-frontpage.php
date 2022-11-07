34a5544af75fee6886c8d87cecac0fd919b32251
Mike-steele7 <102767213+Mike-steele7@users.noreply.github.com>
2022-11-06 19:39:56 -0800

    Update WITS-frontpage.php
    I added the db connection php code to this file!

--- WITS-frontpage.php
+++ WITS-frontpage.php
@@ -1,3 +1,26 @@
+<php> 
+    
+    $host = "webdev.iyaclasses.com";
+    $userid = "ebird_JimJobBob";
+    $userpw = "Treesap3#";
+    $db = "ebird_WITS1";
+
+
+    include '../pdloginvariables.php';
+
+    $mysql = new mysqli(
+        $host,
+        $userid,
+        $userpw,
+        $db
+    );
+
+    if ($mysql->connect_errno) {
+        echo "db connection error : " . $mysql->connect_error;
+        exit();
+
+    }
+    ?>
 <html>
     <head>
         <title>WITS Frontpage</title>
