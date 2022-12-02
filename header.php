f9e83c29b4c827a208ea2b971ea5f7181470988f
Gringl <ezrajbird0@gmail.com>
2022-12-02 01:55:31 -0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <style>
+        #navbar {
+            height: 150px;
+            font-family: "Stretch Pro";
+            font-size: 16px;
+            color: white;
+            position: absolute;
+            margin-top: 30px;
+            margin-left: 1200px;
+            word-spacing: 40px;
+            
+        }
+    </style>
+</head>
+<body style="background-color: black">
+<div id="navbar">
+
+</div>
+</body>
+</html>
