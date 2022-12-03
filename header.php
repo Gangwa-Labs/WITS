1ebe9a42c96959b4c3607f8f35f8d47f146ecfab
Gringl <ezrajbird0@gmail.com>
2022-12-02 16:40:54 -0800

    added log to header

--- header.php
+++ header.php
@@ -20,32 +20,42 @@
 
         .navItem {
         }
-        .navItem:hover{
+
+        .navItem:hover {
             color: #FFCC00;
             transition: 1s;
         }
+
         .navItem a {
             color: inherit;
             text-decoration: none;
 
         }
+        #logoimage {
+            position: absolute;
+            width: 250px;
+            float: left;
+            top: 0;
+        }
+
     </style>
 </head>
 <body>
+<img id="logoimage" src="witslogo.png">
+
 <ul id="navList">
     <div id="navbar">
-
         <div class="navItem">
             <li><a href="about:blank">INFO</a></li>
         </div>
         <div class="navItem">
-            <li><a  href="about:blank">CATALOG</a></li>
+            <li><a href="about:blank">CATALOG</a></li>
         </div>
         <div class="navItem">
-            <li><a  href="about:blank">ACCOUNT</a></li>
+            <li><a href="about:blank">ACCOUNT</a></li>
         </div>
-        <div class="navItem">
-            <li><a  href="about:blank">SEARCH</a></li>
+        <div class="navItem" style="padding-right: 20px">
+            <li><a href="about:blank">SEARCH</a></li>
         </div>
     </div>
 </ul>
